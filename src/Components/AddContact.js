53e19aacf78e9f42ce4d91e93b777cf54449d1f1
Midhun k <midhunmanikandank@gmail.com>
2023-03-10 23:28:10 +0530

    Add flight booking website

---
+++ src/Components/AddContact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+class AddContact extends React.Component {
+    render(){
+        return(
+            <div className='ui main'>
+                <h2>Add Contact</h2>
+                <form className='ui form' >
+                  <div className='feild'>
+                    <label>Name</label>
+                    <input type='text' name='name' placeholder='Name'
+                  </div>
+                </form>
+            </div>
+        )
+    }
+}
